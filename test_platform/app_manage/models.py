7b877a332db2273d3a1c4112a4cb0a7c1a0635c4
buruizhao <buruizhao@163.com>
2020-03-06 19:27:28 +0800

    登陆页面

---
+++ test_platform/app_manage/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    """
+    项目表
+    """
+    name = models.CharField("名称", max_length=100, null=False, default="")
+    remark = models.TextField("备注", default="")
+    status = models.BooleanField("状态", default=True)
+    create_time = models.DateTimeField("创建时间", auto_now_add=True)
+    update_time = models.DateTimeField("更新时间", auto_now=True)
+
---
+++ test_platform/app_personal/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def login(request):
+    # 处理登陆页面
+    if request.method == "GET":
+        return render(request, "login.html")
+
+    # 处理登陆请求
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        if username == "" or password == "":
+            return render(request, "login.html", {
+                "msg":"用户名或密码不能为空！"
+            })
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user) #记录用户的登陆状态
+            return HttpResponseRedirect("/manage/")
+        else:
+            return render(request, "login.html", {
+                "msg":"用户名或密码错误！"
+            })
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/")
+
+@login_required
+def manage(request):
+    return render(request, "manage.html")
