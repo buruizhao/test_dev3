20a2ee117d6d1a91bbda0fda3cc3a3199ffe29b3
buruizhao <buruizhao@163.com>
2020-03-27 19:49:53 +0800

    更新

---
+++ test_platform/app_case/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def list_case(request):
+    return render(request, 'case/debug.html')
--- test_platform/test_platform/urls.py
+++ test_platform/test_platform/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
     #项目/模块管理
     path('manage/', include('app_manage.urls')),
 
+    # 用例管理
+    path('case/', include('app_case.urls')),
+
 
 ]
